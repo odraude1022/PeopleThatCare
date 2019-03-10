b696168977122d0b21dfc44cd112bdf05212e9da
Eduardo Iglesias <eriglesias@ufl.edu>
2019-03-10 11:04:08 -0400

    created user sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  end
