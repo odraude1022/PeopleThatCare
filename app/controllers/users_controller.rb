8b96722ba22df1f1b9183cd20838c386aa6cf4f3
Eduardo Iglesias <eriglesias@ufl.edu>
2019-03-13 20:10:53 -0400

    We changed the names of everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      user_log_in @user
       redirect_to @user
     else
       redirect_to new_user_path, alert: @user.errors.full_messages.to_sentence
