fe242499e9760dc0b34c5868f066f9521dc21121
Andy Weiss <andyweiss1982@gmail.com>
2019-03-18 15:09:34 -0400

    limit to 3 checkboxes

---
+++ app/assets/javascripts/check_boxes.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.category_check_box').on('click', function(){
+    var checked_count = $('.category_check_box:checked').length
+    if (checked_count >= 3) {
+      var unchecked = $('.category_check_box:not(:checked)')
+      $.each(unchecked, function(index, checkbox){
+        $(checkbox).attr("disabled", true)
+      })
+    }else{
+      var checkboxes = $('.category_check_box')
+      $.each(checkboxes, function(index, checkbox){
+        $(checkbox).removeAttr("disabled")
+      })
+    }
+  })
+})
