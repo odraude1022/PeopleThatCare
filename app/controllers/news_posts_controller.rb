836d33729e5748c22b6b2ff62c961467ce8e5d66
Eduardo Iglesias <eriglesias@ufl.edu>
2019-03-18 20:05:48 -0400

    added rails pagination to news_posts

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -60,7 +60,21 @@ class NewsPostsController < ApplicationController
     if charity_logged_in?
       @news_posts = current_charity.news_posts
     elsif user_logged_in?
+<<<<<<< HEAD
       @news_posts = NewsPost.where(charity: current_user.charities)
+=======
+      user_charities = current_user.user_charities
+      @charities = []
+      user_charities.map do |user_charity|
+        charity = Charity.find(user_charity.charity_id)
+        @charities.push(charity)
+      end
+      @news_posts = []
+      @charities.map do |charity|
+        @news_posts.push(charity.news_posts.to_a)
+      end
+      @news_posts.flatten!
+>>>>>>> added rails pagination to news_posts
     end
   end
 
